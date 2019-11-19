362ea2d4ea92925726da82deba35a8dc9ba4299a
Tiandaguang <zhizunwudivip2000@163.com>
2019-11-19 17:19:18 +0800

    nio读取字符串写入文件

--- src/main/java/com/boot/demo/core/nio/TdgNio.java
+++ src/main/java/com/boot/demo/core/nio/TdgNio.java
@@ -1,9 +1,10 @@
 package com.boot.demo.core.nio;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
 
 /**
  * @DATE 2019/11/19 14:15
@@ -24,42 +25,101 @@ import java.nio.channels.FileChannel;
  **/
 public class TdgNio {
 
-    public static void main(String[] args) {
-        ByteBuffer buffer = ByteBuffer.allocate(102);
-        RandomAccessFile aFile = null;
-        RandomAccessFile bFile = null;
+
+    /**
+     * 功能描述: nio读取字符串 然后
+     * <p>
+     * ByteBuffer
+     * <p>
+     * <p>   读写从buffer角度来说
+     * flip    从写到读
+     * <p>
+     * clear   从读到写
+     * compact 从读到写 ,不清空buffer
+     *
+     * @return : void
+     * @param: str     长度为4整数  如果兼容 需要对 不是4截取的做单独处理
+     * @param: fileName
+     * @author : Tiandaguang
+     * @date : 2019/11/19 16:06
+     */
+    public static void readStrAndWrite(String str, String fileName) {
+        if (StringUtils.isEmpty(str)) {
+            return;
+        }
+        //使用buffer
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+        int num = 0;
+        if (str.length() % 4 == 0) {
+            num = str.length() / 4;
+        } else {
+            num = str.length() / 4 + 1;
+        }
         try {
-            aFile = new RandomAccessFile("D:/nio.txt", "rw");
-            bFile = new RandomAccessFile("D:/aa.txt", "rw");
-
-            FileChannel channel = aFile.getChannel();
-
-            int read = channel.read(buffer);
-            while (read != -1) {
+            RandomAccessFile bFile = new RandomAccessFile(fileName, "rw");
+            for (int i = 0; i < num; i++) {
+                String newStr = str.substring(i * (4), (i + 1) * 4);
+                System.out.println(newStr);
+                //清空buffer
+                buffer.put(newStr.getBytes());
                 buffer.flip();
-                if (buffer.hasRemaining()) {
+                while (buffer.hasRemaining()) {
                     bFile.write((char) buffer.get());
                 }
                 buffer.compact();
-                read = channel.read(buffer);
             }
-
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (!buffer.hasRemaining()) {
-                    buffer = null;
-                }
-                if (aFile != null) {
-                    aFile.close();
-                }
-                if (bFile != null) {
-                    bFile.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
+    }
+
+    public static void main(String[] args) {
+        readStrAndWrite("1234432112344321", "E:/eee.txt");
+//        ByteBuffer buffer = ByteBuffer.allocate(1024);
+//        RandomAccessFile aFile = null;
+//        RandomAccessFile bFile = null;
+//        try {
+//            aFile = new RandomAccessFile("D:/nio.txt", "rw");
+//            bFile = new RandomAccessFile("D:/aa.txt", "rw");
+//
+//            FileChannel channel = aFile.getChannel();
+//
+//            int read = channel.read(buffer);
+//            while (read != -1) {
+////                System.out.println("flip前limit-->" + buffer.limit());
+////                System.out.println("flip前position-->" + buffer.position());
+//                buffer.flip();
+////                System.out.println("flip后limit-->" + buffer.limit());
+////                System.out.println("flip后position-->" + buffer.position());
+//                if (buffer.hasRemaining()) {
+//                    bFile.write((char) buffer.get());
+//                }
+//
+////                System.out.println("compact前limit-->" + buffer.limit());
+////                System.out.println("compact前position-->" + buffer.position());
+//                buffer.compact();
+////                System.out.println("compact后 limit-->" + buffer.limit());
+////                System.out.println("compact后 position-->" + buffer.position());
+//                read = channel.read(buffer);
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                if (!buffer.hasRemaining()) {
+//                    buffer = null;
+//                }
+//                if (aFile != null) {
+//                    aFile.close();
+//                }
+//                if (bFile != null) {
+//                    bFile.close();
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 }
