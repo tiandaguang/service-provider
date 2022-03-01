e18a69547ff648654fac8a33425dec9adb1f7cff
Tdg <zhizunwudivip2000@163.com>
2022-03-01 12:04:20 +0800

    增加demo

--- src/main/java/com/boot/demo/DemoApplication.java
+++ src/main/java/com/boot/demo/DemoApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@ImportResource(locations = {"classpath*:/dubbo-*.xml"})
+//@ImportResource(locations = {"classpath*:/dubbo-*.xml"})
 public class DemoApplication {
     // 启动的时候要注意，由于我们在controller中注入了RestTemplate，所以启动的时候需要实例化该类的一个实例
     @Autowired
--- src/main/java/com/boot/demo/controller/DemoController.java
+++ src/main/java/com/boot/demo/controller/DemoController.java
@@ -1,21 +1,29 @@
 package com.boot.demo.controller;
 
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @DATE 2019/11/4 20:33
  * @Description 测试请求类
  * @Author Tian Daguang
  **/
-//@RestController
-//@RequestMapping("demo")
+@RestController
+@RequestMapping("demo")
 @Slf4j
 public class DemoController {
 
     @Autowired
     private RestTemplate restTemplate;
+
 
 //    @Autowired
 //    private EtcMonitorMessageService monitorMessageHandlerImpl;
@@ -48,4 +56,12 @@ public class DemoController {
 //    public JsonResponse to(@RequestBody MonitorMessageRequest request) throws Exception {
 //        return JsonResponse.fail("接收rest请求成功");
 //    }
+
+    @PostMapping("send")
+    public String send() throws Exception {
+        log.info("可以用了！！！");
+        Map<String, Object> mp = new HashMap<>();
+        mp.put("code", 100);
+        return JSON.toJSONString(mp);
+    }
 }
