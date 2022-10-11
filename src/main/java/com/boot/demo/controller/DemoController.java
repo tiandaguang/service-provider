36f37788d1c0a878b244a77b9e9f5dae11541f77
Tdg <zhizunwudivip2000@163.com>
2022-10-11 13:21:12 +0800

    mycat 1.6+mysql 5.1.47  读写分离

--- src/main/java/com/boot/demo/controller/DemoController.java
+++ src/main/java/com/boot/demo/controller/DemoController.java
@@ -1,11 +1,15 @@
 package com.boot.demo.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.boot.demo.entity.TradeBill;
 import com.boot.demo.service.TradeBillService;
+import com.boot.demo.utils.UUIDUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.util.Date;
 
 
 /**
@@ -25,7 +29,19 @@ public class DemoController {
     @GetMapping(path = "send/{id}")
     public String send(@PathVariable Long id) throws Exception {
         log.info("可以用了！！！");
-        return JSON.toJSONString(tradeBillService.getById(id));
+        return JSON.toJSONString(tradeBillService.list());
+    }
+
+
+    @GetMapping(path = "add")
+    public void send() throws Exception {
+        log.info("可以用了！！！");
+        TradeBill bill = new TradeBill();
+        bill.setBusDate("2022-06-30");
+        bill.setMerchantNo(UUIDUtil.generate());
+        bill.setTradeAmounts(new BigDecimal("10.11"));
+        bill.setCreateTime(new Date());
+        tradeBillService.save(bill);
     }
 
 
